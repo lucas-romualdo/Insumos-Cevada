35244128d53a99a284adce6e3dbe564cc7187d19
José Lucas <j.lucas.romualdo@gmail.com>
2024-11-20 19:17:06 -0300

    atualizando a pasta

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/theme/default.js'
+import { GlobalStyle } from './styles/global.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+	<ThemeProvider theme={defaultTheme}>
+		<GlobalStyle/>
+		<App />
+	</ThemeProvider>
+    
   </StrictMode>,
 )
