f869ab37a6229cfcc588dfb7ecd77908c2731792
José Lucas <j.lucas.romualdo@gmail.com>
2024-11-24 22:16:03 -0300

    aula-02

---
+++ src/navbar/cevada/cevada.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const cevada = () =>{
+  return (
+    <div>
+      <h3>Lúpulo</h3>
+      <div>
+        <img src="#/">
+        <img src="#"/>
+      </div>
+    </div>
+  );
+}
+export default cevada;
---
+++ src/navbar/fermentacao/fermentacao.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const fermentacao = () =>{
+  return (
+    <div>
+      <h3>Fermentação</h3>
+      <div>
+        <img src="#"/>
+        <img src="#"/>
+      </div>
+    </div>
+  );
+}
+export default fermentacao;
